bb3e98a9885d0907389211023c118a233c6bbade
Sudipta Sen <sanborn.sen@gmail.com>
2016-01-20 23:42:49 +0530

    facebook access token used. signup api changed

--- resources/signup.py
+++ resources/signup.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, reqparse
 from models.user import User
 from models.token import Token
 from models.device import Device
+from models.appversion import AppVeriosn
 from common.errorhandler import ErrorHandler
 
 
@@ -11,15 +12,7 @@ todos = {}
 post_parser = reqparse.RequestParser(bundle_errors=True)
 errorHandler = ErrorHandler()
 
-post_parser.add_argument('fb_id', type=int, location='form', required=True, help='Facebook id is required')
-post_parser.add_argument('fb_username', location='form', required=True, help='Facebook username is required')
-post_parser.add_argument('fb_email', type=errorHandler.checkEmail, location='form', required=True, help='Facebook email is required')
-post_parser.add_argument('fb_firstname', location='form', required=True, help='Firstname is required')
-post_parser.add_argument('fb_lastname', location='form', required=True, help='Lastname is required')
-post_parser.add_argument('fb_name', location='form', required=True, help='Full name is required')
-post_parser.add_argument('fb_dp', location='form', help='')
-post_parser.add_argument('fb_country', location='form', help='')
-post_parser.add_argument('fb_gender', location='form', help='')
+post_parser.add_argument('fb_token', location='form', required=True, help='Facebook access token is required')
 post_parser.add_argument('device_id', location='form', required=True, help='Device id is required')
 post_parser.add_argument('device_name', location='form', help='')
 
@@ -28,18 +21,21 @@ class Signup(Resource):
 		args = post_parser.parse_args()
 		userModel = User()
 		tokenModel = Token()
-		deviceModel = Device()
-		user = userModel.isUserExist(args['fb_email'])
+		appVersion = AppVeriosn()
+		minv, recentv = appVersion.getLatestVersion()
+		user = userModel.isUserExist(args)
 		if user:
 			token_id = user.devices[0].token_id
 			token = tokenModel.getTokenById(token_id)
 			response = {"token" : token.token}
 		else:
+			deviceModel = Device()
 			newUser = userModel.addUser(args)
 			newToken = tokenModel.createNewToken()
 			args['user_id'] = newUser.id
 			args['token_id'] = newToken.id
 			newDevice = deviceModel.addDevice(args)
 			response = {"token" : newToken.token}
-
+		response['min'] = minv
+		response['recent'] = recentv
 		return response, 200
